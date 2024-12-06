52f0b6efbe7db38002e266e11cf919fa4ec10641
DanieMituku <dberhe490@gmail.com>
2024-12-06 12:55:40 +0300

    first commit

--- e2e/prep.spec.js
+++
@@ -1,6 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test('Navigate to the link', async ({page})=>{
-    await page.goto('https://auditorium.ai/')
-    await expect(page).toHaveTitle(/Auditorium/)
-}) 
